e597be4adc5c8065ec38ec71e9f0d105911c2186
Jason Crawford <daxapps777@gmail.com>
2019-05-31 08:55:43 -0400

    complete excercise

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
 import Home from "./screens/Home";
+import CameraScreen from "./screens/CameraScreen";
 
 const MainNavigator = createStackNavigator(
   {
-    Home: {screen: Home}
+    Home: {screen: Home},
+    Camera: {screen: CameraScreen}
   },
   {
     defaultNavigationOptions: {
--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default class Home extends React.Component {
+
+  static navigationOption = {
+    title: "PhotoClicker"
+  }
+
   render() {
+
+    let photo = this.props.navigation.getParam("photo", "empty");
+
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <Image
+        resizeMode="center"
+        style={styles.imageHolder}
+        source={
+          photo === "empty" ? require("../assets/email.png") : photo
+        }
+        />
+        <Button
+        title="Take Photo"
+        style={styles.button}
+        onPress={() => {
+          this.props.navigation.navigate("Camera");
+        }}
+        />
       </View>
     );
   }
@@ -18,4 +39,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  imageHolder: {
+    alignSelf: "center",
+    height: 500,
+    margin: 20
+  },
+  button: {
+    margin: 20
+  }
 });
